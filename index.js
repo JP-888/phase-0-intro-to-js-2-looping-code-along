c1946baa52490eff1d154022a1636a0b9b8e20a4
JP <jpsimshed@gmail.com>
2021-05-07 10:38:16 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function countDown(count) {
+    for (let i = count; i >= 0; i--) {
+        console.log(i)
+    }
+}
+
+function writeCards(names, greeting) { 
+    const result = [];
+    for (let i = 0; i < names.length; i++) {
+        result.push(`Thank you, ${names[i]}, for the wonderful ${greeting} gift!`)
+    }
+    return result   
+}
